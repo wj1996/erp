35f9ac20357e21acee6828380fd3bce54fd5cc0f
jie8023yu <2425393776@qq.com>
2018-12-25 09:11:04 +0800

    2018/12/25

--- bw2/erp_parent/erp_web/src/main/webapp/js/report.js
+++ bw2/erp_parent/erp_web/src/main/webapp/js/report.js
@@ -7,7 +7,12 @@ $(function(){
 		columns:[[
 			{field:'name',title:'商品类型',width:100},
 			{field:'y',title:'销售额',width:100}
-		]]
+		]],
+		singleSelect:true,
+		onLoadSuccess:function(data){
+			//显示图
+			showChart(data.rows);
+		}
 	})
 	
 	$("#btnSearch").bind("click",function(){
@@ -18,5 +23,73 @@ $(function(){
 		$("#grid").datagrid("load",formData);
 	})
 	
-	
 })
+
+function showChart(_data){
+	Highcharts.chart('container', {
+	    chart: {
+	        plotBackgroundColor: null,
+	        plotBorderWidth: null,
+	        plotShadow: false,
+	        type: 'pie'
+	    },
+	    title: {
+	        text: '销售统计'
+	    },
+	    tooltip: {
+	        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+	    },
+	    //信用
+	    credits:{enabled:false},
+	    //导出
+	    exporting:{enabled:true},
+	    plotOptions: {
+	        pie: {
+	            allowPointSelect: true,
+	            cursor: 'pointer',
+	            dataLabels: {
+	                enabled: true,
+	                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+	                style: {
+	                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+	                }
+	            }
+	        }
+	    },
+	    series: [{
+	        name: '比较',
+	        colorByPoint: true,
+	       /* data: [{
+	            name: 'Chrome',
+	            y: 61.41,
+	            sliced: true,
+	            selected: true
+	        }, {
+	            name: 'Internet Explorer',
+	            y: 11.84
+	        }, {
+	            name: 'Firefox',
+	            y: 10.85
+	        }, {
+	            name: 'Edge',
+	            y: 4.67
+	        }, {
+	            name: 'Safari',
+	            y: 4.18
+	        }, {
+	            name: 'Sogou Explorer',
+	            y: 1.64
+	        }, {
+	            name: 'Opera',
+	            y: 1.6
+	        }, {
+	            name: 'QQ',
+	            y: 1.2
+	        }, {
+	            name: 'Other',
+	            y: 2.61
+	        }]*/
+	        data:_data
+	    }]
+	});
+}
